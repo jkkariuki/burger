8f065ee72d952c8ec8c0b448cae8e55341f75918
jkkariuki <jkkariuki15@gmail.com>
2017-12-04 22:50:25 -0500

    updated

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "james3",
   password: "password",
   database: "burgers_db"
-});
+  });
+};
+
+connection.connect();
 
 // Make connection.
 connection.connect(function(err) {
