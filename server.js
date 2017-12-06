c5957213c083ce59254b39d1b3a470b68940030f
jkkariuki <jkkariuki15@gmail.com>
2017-12-05 19:10:29 -0500

    updated

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var connection = require("./config/connection.js");
 var port = process.env.PORT || 3000;
-
 var app = express();
-
-app.use(express.static(process.cwd() + '/public'));
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(methodOverride('X-HTTP-Method-Override'));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
 
-var routes = require('./controllers/burgers_controller.js');
-
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(port);
